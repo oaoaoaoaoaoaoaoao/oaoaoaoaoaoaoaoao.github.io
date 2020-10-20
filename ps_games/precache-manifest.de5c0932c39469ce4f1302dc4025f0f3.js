f72ba8af985f10803c2a30706ad0cdf114f03782
oaoaoaoaoaoaoaoao <59417661+oaoaoaoaoaoaoaoao@users.noreply.github.com>
2020-10-20 14:29:22 +0600

    Add files via upload

---
+++ ps_games/precache-manifest.de5c0932c39469ce4f1302dc4025f0f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92cf71e28ab50406ee68",
+    "url": "/static/css/main.f5bd6ab3.chunk.css"
+  },
+  {
+    "revision": "92cf71e28ab50406ee68",
+    "url": "/static/js/main.92cf71e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "79abb698bdcb6dedc6d3",
+    "url": "/static/css/2.3032cfa9.chunk.css"
+  },
+  {
+    "revision": "79abb698bdcb6dedc6d3",
+    "url": "/static/js/2.79abb698.chunk.js"
+  },
+  {
+    "revision": "b903cd22e7270f2d04e3f670719d3a18",
+    "url": "/index.html"
+  }
+];
