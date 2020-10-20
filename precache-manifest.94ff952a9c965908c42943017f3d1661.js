6a4f9f474fbd1018acea6342af5e1aadae5d20cc
oaoaoaoaoaoaoaoao <59417661+oaoaoaoaoaoaoaoao@users.noreply.github.com>
2020-10-20 12:30:24 +0600

    Add files via upload

---
+++ precache-manifest.94ff952a9c965908c42943017f3d1661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99d522747342be81d165",
+    "url": "./static/css/main.d5446e99.chunk.css"
+  },
+  {
+    "revision": "99d522747342be81d165",
+    "url": "./static/js/main.ab5b58a9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "2b913b8c62dabf1a9355",
+    "url": "./static/css/2.bd609757.chunk.css"
+  },
+  {
+    "revision": "2b913b8c62dabf1a9355",
+    "url": "./static/js/2.a18bc010.chunk.js"
+  },
+  {
+    "revision": "0fdb188d5d5fa859492f59f526d9899d",
+    "url": "./index.html"
+  }
+];
