3d0b7d91098c71ac2da7816ca8a4c8cd6a3b4e1b
oaoaoaoaoaoaoaoao <59417661+oaoaoaoaoaoaoaoao@users.noreply.github.com>
2020-11-02 16:34:35 +0600

    Add files via upload

---
+++ almaz/precache-manifest.274a75136677e8a3875d91e84ccd5374.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7a1e420e765e5e8b5eb",
+    "url": "/static/css/main.9d203010.chunk.css"
+  },
+  {
+    "revision": "d7a1e420e765e5e8b5eb",
+    "url": "/static/js/main.a35c2c8b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d3e8097de2c2d96fa58e",
+    "url": "/static/css/2.2d6169ee.chunk.css"
+  },
+  {
+    "revision": "d3e8097de2c2d96fa58e",
+    "url": "/static/js/2.37c206c6.chunk.js"
+  },
+  {
+    "revision": "2cdf55dc63693a4b5636bd57231f9fa0",
+    "url": "/static/media/click.2cdf55dc.png"
+  },
+  {
+    "revision": "8d3b1c63a3d4d50242264462f58199ff",
+    "url": "/index.html"
+  }
+];
